5b8bd7ac185762514ec373ab4db21f7466c2aecd
lander247 <electronicmail837@gmail.com>
2024-12-30 15:47:46 -0700

    added the thing

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"sb-battery",	5,	3},
+	{"┇ ",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
 };
 
