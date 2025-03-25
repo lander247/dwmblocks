bdddc73732544403887707d533d2591918da61f0
lander247 <electronicmail837@gmail.com>
2025-03-25 16:02:01 -0600

    changes cuz nerdfontz for nerdz..

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"┇ ",	"sb-battery",	5,	3},
+	{"| ",	"sb-battery",	5,	3},
+	{"",	"sb-volume",	0,	10},
 	{"",	"sb-clock",	60,	1},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " ┇ ";
+static char delim[] = " | ";
 static unsigned int delimLen = 5;
